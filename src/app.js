0f5098ff726b709007ce8db62b7c85f33973e96e
ryantsai <rtsai0104@gmail.com>
2019-05-11 14:34:22 -0500

    Add react project

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 // require('./db/mongoose');
 
 const app = express();
-const PORT = process.env.port || 3000;
+const PORT = process.env.port || 5000;
 
 app.get('', (req, res) => {
   res.send('HI');
