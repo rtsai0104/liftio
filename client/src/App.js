8de77d172483a778fcc419b804b46a07b36b341a
rtsai0104 <rtsai0104@gmail.com>
2019-05-11 15:24:42 -0500

    Working express + react

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Provider } from 'react-redux'
+import logo from './logo.svg'
+import './App.css'
+import store from './store'
+import Customers from './components/Customer/customers'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render () {
+    return (
+      <Provider store={ store }>
+        <div className="App">
+          <header className="App-header">
+            <img src={ logo } className="App-logo" alt="logo"/>
+            <h1 className="App-title">React/Redux Express Starter</h1>
+          </header>
+          <Customers/>
+        </div>
+      </Provider>
+    )
+  }
 }
 
-export default App;
+export default App
--- src/app.js
+++ src/app.js
@@ -8,6 +8,16 @@ app.get('', (req, res) => {
   res.send('HI');
 });
 
+app.get('/api/customers', (req, res) => {
+  const customers = [
+    {id: 1, firstName: 'John', lastName: 'Doe'},
+    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
+    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
+  ];
+
+  res.json(customers);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is up on port ${PORT}`);
 });
