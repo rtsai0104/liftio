646116f9dbcdbe255eccc0961b7ea71e2d99ed37
Pawtang <ben.dibuz@gmail.com>
2019-05-11 20:13:42 +0000

    init push

--- src/app.js
+++ src/app.js
@@ -1,12 +1,28 @@
 const express = require('express');
-// require('./db/mongoose');
-
 const app = express();
-const PORT = process.env.port || 5000;
+const path = require('path');
+const router = express.Router();
+const PORT = process.env.port || 3000;
 
-app.get('', (req, res) => {
-  res.send('HI');
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
 });
+
+router.get('/about',function(req,res){
+  res.sendFile(path.join(__dirname+'/about.html'));
+});
+
+router.get('/sitemap',function(req,res){
+  res.sendFile(path.join(__dirname+'/sitemap.html'));
+});
+
+//add the router
+app.use('/', router);
+app.use(express.static(__dirname + '/views'));
+//Store all HTML files in view folder.
+app.use(express.static(__dirname + '/scripts'));
+//Store all JS and CSS in Scripts folder.
 
 app.listen(PORT, () => {
   console.log(`Server is up on port ${PORT}`);
