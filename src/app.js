86427ba41f3c5cb592408163e7501ecd4985f68f
Ryan Tsai <rtsai0104@gmail.com>
2019-06-06 18:26:11 -0500

    Start adding user auth in server code

--- src/app.js
+++ src/app.js
@@ -1,40 +1,12 @@
 const express = require('express');
+require('./db/mongoose');
+const userRouter = require('./routers/user');
+// const taskRouter = require('./routers/task');
+
 const app = express();
-const path = require('path');
-const router = express.Router();
-const PORT = process.env.port || 5000;
 
 app.use(express.json());
+app.use(userRouter);
+// app.use(taskRouter);
 
-app.get('', (req, res) => {
-  res.send('HI');
-});
-
-app.post('/users/login', async (req, res) => {
-  setTimeout(() => {
-    res.status(201).send({
-      login: 'success'
-    });
-  }, 4000);
-
-});
-
-app.post('/users/signup', async (req, res) => {
-  res.send({
-    signup: 'success'
-  });
-});
-
-app.get('/api/customers', (req, res) => {
-  const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
-  ];
-
-  res.json(customers);
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is up on port ${PORT}`);
-});
+module.exports = app;
---
+++ src/routers/user.js
@@ -0,0 +1,106 @@
+const express = require('express');
+// const multer = require('multer'); /
+const router = new express.Router();
+const auth = require('../middleware/auth');
+
+router.post('/users', async (req, res) => {
+
+  const user = new user(req.body);
+  try {
+    await user.save();
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
+  } catch (e) {
+    res.status(400).send(e);
+  }
+
+});
+
+router.post('/users/login', async (req, res) => {
+
+  try {
+    const user = await User.findByCredentials(req.body.email, req.body.password);
+    const token = await user.generateAuthToken();
+    res.send({ user: user, token });
+  } catch (e) {
+    res.status(400).send({ error: 'Unable to log in' });
+  }
+
+});
+
+router.post('/users/logout', auth, async (req, res) => {
+
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => token.token !== req.token );
+    await req.user.save();
+    res.send({ msg: 'Succesfully logged out' });
+  } catch (e) {
+    res.status(500).send({ error: 'Error logging out' });
+  }
+
+});
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+
+  try {
+    req.user.tokens = []
+    await req.user.save();
+    res.send({ msg: 'Succesfully logged out of all accounts' });
+  } catch (e) {
+    res.status(500).send({ error: 'Error logging out' });
+  }
+
+});
+
+router.get('/users', async (req, res) => {
+
+  if (req.header('Tsai') !== 'Tsai') {
+    return res.status(401).send({ error: 'Invalid' });
+  }
+
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+
+});
+
+router.get('/users/me', auth, async (req, res) => {
+
+  res.send(req.user);
+});
+
+router.patch('/users/me', auth, async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['name', 'email', 'password', 'age'];
+  const isValidOperation = updates.every((property) => allowedUpdates.includes(property));
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid property" });
+  }
+
+  try {
+    updates.forEach((update) => req.user[update] = req.body[update] );
+    await req.user.save();
+    res.send(req.user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+
+});
+
+router.delete('/users/me', auth, async (req, res) => {
+
+  try {
+
+    await req.user.remove();
+    sendCancelationEmail(req.user.email, req.user.name);
+    res.send(req.user);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+module.exports = router;
